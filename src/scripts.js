37d159993fcf4e4b5fe08da7a4d3ac871ab3c15f
Kevin Hartmann <kevinhartmann23@gmail.com>
2021-01-14 21:16:53 -0700

    displayed banner

--- src/scripts.js
+++ src/scripts.js
@@ -1,14 +1,17 @@
 //Query Selectors
 let displayGrid = document.querySelector('.card-display');
-let searchButton = document.querySelector('.search')
+let searchButton = document.querySelector('.search');
 let searchInput = document.querySelector('.search-input');
-
+let nomCount = document.querySelector('.nomination-count');
+let bannerDisplay = document.querySelector('.banner-display');
+let introTitle = document.querySelector('.intro-title')
 //EVENT HANDLERS
 searchButton.addEventListener('click', submitSearch)
 
 //GLOBALS
 let movieData = [];
 let nominatedFilms = [];
+let nominationsLeft = 4;
 
 // FETCH REQUESTS
 
@@ -27,7 +30,18 @@ function submitSearch(){
 }
 
 function nominateMovie(event){
-  return nominatedFilms.push(movieData.find(movie => movie.imdbID === event.target.id))
+  if(nominatedFilms.length < 4){
+    event.target.classList.add('disabled')
+    event.target.classList.innerHTML = `Nominated<img id="${event.target.id}" class="nominate" src="./assets/nominated.png" alt="movie nominated for an award">`
+    nomCount.innerText = `${nominationsLeft - nominatedFilms.length} Nominations Left...`
+    nominatedFilms.push(movieData.find(movie => movie.imdbID === event.target.id))
+  } else if (nominatedFilms.length === 4) {
+    nominatedFilms.push(movieData.find(movie => movie.imdbID === event.target.id))
+    nomCount.innerText = `${nominationsLeft - nominatedFilms.length} Nominations Left...`
+    displayGrid.classList.add('hidden')
+    displayBanner()
+    bannerDisplay.classList.remove('hidden')
+  }
 }
 
 
@@ -53,5 +67,27 @@ function createGrid(){
 
   nominateButton.forEach(button => {
     button.addEventListener('click', nominateMovie)
+  })
+}
+
+function displayBanner(){
+  searchInput.classList.add('hidden')
+  searchButton.classList.add('hidden')
+  introTitle.innerText = "Your Nominations Are..."
+  nomCount.innerText = "Thanks for your Nominations!"
+  bannerDisplay.innerHTML = ''
+  nominatedFilms.forEach(nom => {
+    bannerDisplay.innerHTML +=  `
+      <article class="nominated-card">
+          <div class="image-wrapper">
+            <img class="nom-image" src="${nom.Poster}" alt="nominate for an award">
+          </div>
+            <h2 class="nom-title">${nom.Title}</h4>
+            <h3 class="nom-year">Year: ${nom.Year}</h4>
+            <div class="nom-foot">
+            <h4>Nominated</h4>
+            </div>
+      </article>
+      `
   })
 }
